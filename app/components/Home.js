ce22254b162ffe7316e6ae76dc6b8cd93f982ac2
Chuck4Git <JoshGC3@gmail.com>
2015-10-20 09:44:47 -0400

    React Project

---
+++ app/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component
+{
+	render()
+	{
+		return (
+			<h2 className = "text-center">
+				Search by Github Username Above
+			</h2>
+		)
+	}
+};
+
+
+
+export default Home;
